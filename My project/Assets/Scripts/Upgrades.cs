67572db584443739dac77e7429691ef693ae551f
JosephBlom <24.joseph.blom@ksd.org>
2024-01-29 14:01:05 -0800

    Began Tower Upgrades

---
+++ My project/Assets/Scripts/Upgrades.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    [SerializeField] GameObject Bullet;
+    public UpgradePath1 upgradePath1;
+    public UpgradePath2 upgradePath2;
+    public UpgradePath3 upgradePath3;
+}
